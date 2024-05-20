fbd8d0595bf09e080b909f667401cb07bc8895b8
Aniket Gauda <122367767+AniketGauda@users.noreply.github.com>
2024-05-21 05:27:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-//your code here
+// let arr = [3, 3, 4, 2, 4, 4, 2, 4, 4];
+
 function majorityElement(arr){
     let ans = [];
     arr.forEach((elt,idx)=>{
@@ -20,13 +21,13 @@ function majorityElement(arr){
     for(let i=0;i<ans.length;i++){
         let obj = ans[i];
         for(let key in obj){
-            if(obj[key]>Math.floor(ans.length/2))
-                fans = key;
+            if(obj[key]>Math.floor(arr.length/2))
+                fans = key
         }
     }
+
     return fans;
+    // console.log(ans);
 }
 
-
-
-
+// console.log(majorityElement(arr));
