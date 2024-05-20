b090b9fa8bb022e681f8b6f7ee6e9f7af967e609
Aniket Gauda <122367767+AniketGauda@users.noreply.github.com>
2024-05-20 11:59:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
 //your code here
+function majorityElement(arr){
+    let ans = [];
+    arr.forEach((elt,idx)=>{
+        let obj = {};
+        let bool = true;
+        ans.forEach((object)=>{
+            if(elt in object){
+                object[elt]++;
+                bool = false;
+            }
+        });
+        if(bool){
+            obj[elt] = 1;
+            ans.push(obj);
+        }
+    });
+
+    let fans;
+    for(let i=0;i<ans.length;i++){
+        let obj = ans[i];
+        for(let key in obj){
+            if(obj[key]>Math.floor(ans.length/2))
+                fans = key;
+        }
+    }
+    return fans;
+}
+
+
+
+
